4a2ffa19f898e38329bb2e396a91ebc8d6c92fb9
muradoff101 <51491047+muradoff101@users.noreply.github.com>
2021-04-26 18:20:16 +0500

    Update settings.py

--- sergi/settings.py
+++ sergi/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'obshotl14r_f92=3-_!f5-)z*b!8z)3^ec+bk$f4@1pt4j3_r!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
